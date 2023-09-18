31035cac1672959a732a2fadc52f2dbe6215cb30
evaZQR <2539906978@qq,com>
2023-09-18 19:29:17 +0800

    。。

--- tool/E_mail.py
+++ tool/E_mail.py
@@ -1,42 +1,16 @@
 import smtplib
 from email.mime.text import MIMEText
+from dotenv import load_dotenv
+import os
+load_dotenv()
+import os
+MAIL_HOST = os.getenv('MAIL_HOST',"")
+MAIL_USER = os.getenv('MAIL_USER',"")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD","")
+SENDER = os.getenv("SENDER","")
+#print(MAIL_HOST,MAIL_USER,MAIL_PASSWORD,SENDER)
 #设置服务器所需信息
 #163邮箱服务器地址
-def send_email(Message,add):
-    mail_host = 'smtp.163.com'  
-    #163用户名
-    mail_user = 'm19960709068@163.com'  
-    #密码(部分邮箱为授权码) 
-    mail_pass = 'YJQWZHGZNHDVUIRF'   
-    #邮件发送方邮箱地址
-    sender = '19960709068@163.com'  
-    #邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
-    receivers = [add]  
-
-    #设置email信息
-    #邮件内容设置
-    message = MIMEText(Message,'plain','utf-8')
-    #邮件主题       
-    message['Subject'] = 'EVA' 
-    #发送方信息
-    message['From'] = sender 
-    #接受方信息     
-    message['To'] = receivers[0]  
-
-    #登录并发送邮件
-    try:
-        smtpObj = smtplib.SMTP_SSL(mail_host)
-        #登录到服务器
-        smtpObj.login(mail_user,mail_pass) 
-        #发送
-        smtpObj.sendmail(
-            sender,receivers,message.as_string()) 
-        #退出
-        smtpObj.quit() 
-        print('success')
-    except smtplib.SMTPException as e:
-        print('error',e) #打印错误
-
 class SendEmail():
     def __init__(self):
         self.description = "<send email> to use this tool you need know the address of the receiver, and the message you want to send"
@@ -56,13 +30,13 @@ class SendEmail():
         return SendEmail.run(message,address)
     @staticmethod
     def run(message,address): 
-        mail_host = 'smtp.163.com'  
+        mail_host = MAIL_HOST 
         #163用户名
-        mail_user = 'm19960709068@163.com'  
+        mail_user = MAIL_USER
         #密码(部分邮箱为授权码) 
-        mail_pass = 'YJQWZHGZNHDVUIRF'   
+        mail_pass = MAIL_PASSWORD
         #邮件发送方邮箱地址
-        sender = '19960709068@163.com'  
+        sender = SENDER
         #邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
         receivers = [address]  
 
@@ -70,7 +44,7 @@ class SendEmail():
         #邮件内容设置
         message = MIMEText(message,'plain','utf-8')
         #邮件主题       
-        message['Subject'] = 'EVA' 
+        message['Subject'] = 'EVA'  # 可以在这个地方改你的邮件主题 
         #发送方信息
         message['From'] = sender 
         #接受方信息     
@@ -88,8 +62,8 @@ class SendEmail():
             smtpObj.quit() 
             return "send email successfully"
         except smtplib.SMTPException as e:
-            print('error',e) #打印错误
-            return f"send email failed because of the {e}"
+            #print('error',e) #打印错误
+            return f"send email failed because of the ￥￥{e}￥￥"
 if __name__ == '__main__':
     jsonEVA = {
         "message": "Good Evening",
